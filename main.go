7e40983185d561c6ad014a4f79ac7a55dd9e46b2
Dean Wan <deanwan14@gmail.com>
2023-12-19 12:42:29 +1100

    feat: orchestrate code through main.go

--- main.go
+++ main.go
@@ -39,7 +39,13 @@ type Controller struct {
 }
 
 func main() {
+	l := LogAdapter(LogOutput)
+	ds := NewSimpleDataStore()
+	logic := NewSimpleLogic(l, ds)
+	c := NewController(l, logic)
 
+	http.HandleFunc("/hello", c.SayHello)
+	http.ListenAndServe(":8080", nil)
 }
 
 // These are business logic function
